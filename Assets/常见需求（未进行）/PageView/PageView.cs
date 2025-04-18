b0f15140532f2e77eab4932919496ea068753b99
NRatel <36918549+NRatel@users.noreply.github.com>
2025-04-18 15:02:08 +0800

    PageView 要点

--- Assets/常见需求（未进行）/PageView/PageView.cs
+++ Assets/常见需求（未进行）/PageView/PageView.cs
@@ -5,51 +5,58 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using NRatel.Fundamental;
 
-//要点
-// 1、paddingLeft、paddingRight 不需要手动设置了。
-// 因为，要保证第一个和最后一个Cell可以处于Viewport的中心。
-// 所以，需要强制 paddingLeft = paddingRight = (Viewport宽度 - Cell宽度)/2。
-// 这个计算方式对“Cell宽度 <= Viewport宽度”和“Cell宽度 > Viewport宽度”的情况都是适用的。
-// 但需要注意，当“Cell宽度 > Viewport宽度”时，paddingLeft 和 paddingRight 均为负。此时左右边界不能完全显示出，PageView只有设置成 Loop 才有意义。
+//要点：
+// 1、自动吸附对齐（Snap）
+//      滑动放手后，要自动将Cell吸附对齐到Viewport中心，没有自动吸附对齐，就没有Page的概念。
+//      滑动放手后，将吸附到哪一页，可由“放手时位置”由“放手时速度”共同决定。
+//      如将策略定为：
+//          若放手时中心页已变，则吸附到新页。
+//          若放手时中心页未变，则根据放手速度矢量决定是否翻页（超过阈值则翻页）。
 
-// 2、spacingX 有两种处理方式
-// 方式一、仍由用户指定
-// 方式二、强制指定为 Viewport宽度 - Cell宽度，即 每个Cell独占一页
-// 
-//    考虑情况
-//    情况一、Cell宽度 <= Viewport宽度，即正常情况。此时可以强制，
-//          paddingLeft = paddingRight = (Viewport宽度 - Cell宽度)/2；
-//          spacingX = Viewport宽度 - Cell宽度。
-//    情况二、Cell宽度 > Viewport宽度，不太正常，看怎么处理了，
-//          处理方式一、同情况一中处理，结果：spacingX为负，相邻会重叠压住。（用户自行对该情况负责）  
-//          处理方式二、paddingLeft = paddingRight = (Viewport宽度 - Cell宽度)/2； spacingX = 0。能够保证
-//          处理方式三、强制检查，禁止这种情况发生。
+// 2、是否循环翻页（Loop）
+//      可配置 是/否
 
-// 3、自动Snap: 放手后Snap。由当前位置和放手速度共同决定。
-//    若放手时中心页未变，则根据放手速度确定是否翻到前一页或下一页。
+// 3、是否自动轮播（Carousel）
+//      可配置 是/否，滑动速度、页停留时间
 
-// 4、spacingX
+// 4、页宽。
+//      将 Cell宽度 作为页宽，而非 Viewport宽度。
+//      因为，需要支持 Viewport 中同时出现多个Cell。此时最靠近Viewport中心的那个与Viewport中心对齐。
+//      注意，无需支持 Cell宽度 > Viewport宽度。因为此时 Viewport中无法完整展示一个Cell，也难翻到下一页（暂无实际需求）。
 
-// 5、loop: 可配置是否循环翻页
+// 5、paddingLeft、paddingRight。
+//      根据是否开启loop，设定不同。
+//      ①、不开启loop时，
+//          必须固定为：paddingLeft = paddingRight = (Viewport宽度 - Cell宽度)/2。
+//          因为要保证第一个和最后一个Cell能够处于Viewport的中心。
+//          这个计算方式对“Cell宽度 <= Viewport宽度”和“Cell宽度 > Viewport宽度”的情况都是适用的。
+//      ②、开启loop时，
+//          保持用户指定值。但应满足：？
+//          因为循环显示，所有没有“边界”，自然就没有“边距”可言。
+//          但实际上，它还影响首页初始位置。
 
-// 6、carousel: 可配置轮播
+// 6、spacingX
+//      有两种选择
+//      ①、保持用户指定值。但应满足：spacingX <= Viewport宽度 - Cell宽度。
+//      ②、直接强设为：spacingX = Viewport宽度 - Cell宽度，即 每个Cell独占一页
 
 namespace NRatel
 {
-    //2020.4.2 考虑不用List的方式，而是直接实现滑动接口并用dotween处理移动
     public class PageView : ListViewV2
     {
-        ////调整边距
-        //protected override void FixPadding()
-        //{
-        //    paddingLeft = paddingRight = (viewportRT.rect.width - cellPrefabRT.rect.width) / 2;
-        //}
+        public bool snap;
 
-        ////调整间距
-        //protected override void FixSpacingX()
-        //{
-        //    spacingX = viewportRT.rect.width - cellPrefabRT.rect.width;
-        //}
+        //调整边距
+        protected override void FixPadding()
+        {
+            paddingLeft = paddingRight = (viewportRT.rect.width - cellPrefabRT.rect.width) / 2;
+        }
+
+        //调整间距
+        protected override void FixSpacingX()
+        {
+            //spacingX = viewportRT.rect.width - cellPrefabRT.rect.width;
+        }
     }
 }
 
