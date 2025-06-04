ea0a7eecf9f2fe05c2bfd08470b374053a5984a1
NRatel <nratel@qq.com>
2025-06-04 15:18:10 +0800

    Update PageView.cs

--- Assets/基本排布原理（完成）/PageView/PageView.cs
+++ Assets/基本排布原理（完成）/PageView/PageView.cs
@@ -7,54 +7,6 @@ using NRatel.Fundamental;
 using System;
 using System.Reflection;
 
-//要点：
-// 1、自动吸附对齐（Snap）
-//      滑动放手后，要自动将Cell吸附对齐到Viewport中心，没有自动吸附对齐，就没有Page的概念。
-//      滑动放手后，将吸附到哪一页，可由“放手时位置”由“放手时速度”共同决定。
-//      如将策略定为：
-//          若放手时中心页已变，则吸附到新页。
-//          若放手时中心页未变，则根据放手速度矢量决定是否翻页（超过阈值则翻页）。
-
-// 2、是否首尾无限循环（Loop）
-//      可配置 是/否
-
-// 3、是否自动轮播（Carousel）
-//      可配置 是/否，轮播翻页速度、页停留时间
-
-// 4、页宽。
-//      将 Cell宽度 作为页宽，而非 Viewport宽度。
-//      因为，需要支持 Viewport 中同时出现多个Cell。此时最靠近Viewport中心的那个与Viewport中心对齐。
-//      注意，无需支持 Cell宽度 > Viewport宽度。因为此时 Viewport中无法完整展示一个Cell，也难翻到下一页（暂无实际需求）。
-
-// 5、paddingLeft、paddingRight。
-//      根据是否开启loop，设定不同。
-//      ①、不开启loop时，
-//          必须固定为：paddingLeft = paddingRight = (Viewport宽度 - Cell宽度)/2。
-//          因为要保证第一个和最后一个Cell能够处于Viewport的中心。
-//          这个计算方式对“Cell宽度 <= Viewport宽度”和“Cell宽度 > Viewport宽度”的情况都是适用的。
-//      ②、开启loop时，
-//          保持用户指定值。但应满足：？
-//          因为循环显示，所有没有“边界”，自然就没有“边距”可言。
-//          但实际上，它还影响首页初始位置。
-
-// 6、spacingX
-//      有两种选择
-//      ①、保持用户指定值。但应满足：spacingX <= Viewport宽度 - Cell宽度。
-//      ②、直接强设为：spacingX = Viewport宽度 - Cell宽度，即 每个Cell独占一页
-
-// 7、Snap 实现：
-//      ①、找离Viewport中心最近的那个Cell
-//      ②、与惯性速度的冲突
-//      ③、与回弹的冲突
-
-// 8、Carousel 实现：
-//      ①、协程中等待计时
-
-// 9、BeginDrag、EndDrag、Snap、Carousel 的闭环衔接
-
-// 10、开启 loop 对 Snap 和 Carousel 的影响
-//      ①、因为 loop 会重置 Content 的位置，所以 Snap 和 Carousel 时的移动插值不能是 “从当前位置到目标点”了，而是要变成“累计移动量”
-
 namespace NRatel
 {
     public class PageView : ListViewV2, IBeginDragHandler, IEndDragHandler
